25ddc4f98094357046c0a7da2d439415c29dbcda
flyingdev <developinghuman1015@gmail.com>
2014-12-10 10:30:28 +0800

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  has_many :received_messages, :foreign_key => :receiver, :class => 'Message'
+  has_many :sent_messages, :foreign_key => :sender, :clas => 'Message'
   def full_name
     "#{first_name} #{last_name}"
   end
